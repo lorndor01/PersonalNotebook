18de59f2567c467a3ac4157ce8dab3938cc7828f
Liam Orndorff <lorndor01@gmail.com>
2022-09-20 10:59:59 -0400

    added firebase to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'personalnotebook-e7ef3',
+    appId: '1:167781389962:web:12b03c6a49d8f7d589ff73',
+    storageBucket: 'personalnotebook-e7ef3.appspot.com',
+    apiKey: 'AIzaSyDzVOdiAAzCIqH5tm0dC5GduWmwadWZsaU',
+    authDomain: 'personalnotebook-e7ef3.firebaseapp.com',
+    messagingSenderId: '167781389962',
+    measurementId: 'G-XQ15BR19YT',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    projectId: 'personalnotebook-e7ef3',
+    appId: '1:167781389962:web:12b03c6a49d8f7d589ff73',
+    storageBucket: 'personalnotebook-e7ef3.appspot.com',
+    apiKey: 'AIzaSyDzVOdiAAzCIqH5tm0dC5GduWmwadWZsaU',
+    authDomain: 'personalnotebook-e7ef3.firebaseapp.com',
+    messagingSenderId: '167781389962',
+    measurementId: 'G-XQ15BR19YT',
+  }
 };
 
 /*
